e9dc447a0e4612b41b891e655d5823783a3c4db6
EC2 Default User <ec2-user@ip-172-31-2-45.ap-northeast-1.compute.internal>
2023-05-02 03:48:28 +0000

    Update ユーザー認証機能と投稿機能の追加

--- meshiterro/app/controllers/application_controller.rb
+++ meshiterro/app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource) ##デフォルトの場合はroot_pathになってる
+    post_images_path
+  end
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
+  protected
+
+# configureは「設定する」という他動詞,だからconfigure_permitted_parametersは「許可されたパラメータを設定する」になる
+  def configure_permitted_parameters
+    # deviseは考案するの意味、だからdevise_parameter_sanitizerは「パラメータサニタイズ関数を考案する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
+  # resourcesはメソッドで、ルーティングを一括して自動生成してくれる機能を持つ
+  devise_for :users
+  get "homes/about" => "homes#about", as: 'about'
+  resources :post_images, only: [:new, :index, :show, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
